2e1512394029fa1e3a07f0997f3c1a4e8304ca75
Claire Fritzler <claire@tribot.org>
2021-04-03 15:37:30 -0700

    Add list view for FeedLog

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import FeedLog
+from .serializers import FeedLogSerializer
+from rest_framework import generics
 
-# Create your views here.
+class FeedLogListView(generics.ListCreateAPIView):
+    queryset = FeedLog.objects.all()
+    serializer_class = FeedLogSerizlizer
+    
