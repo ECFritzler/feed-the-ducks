3bfb902cdc4e65213c42550689f55cb0ef7b445a
Claire Fritzler <claire@tribot.org>
2021-04-04 17:18:15 -0700

    Remove date field from backend

--- backend/core/models.py
+++ backend/core/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 class FeedLog(models.Model):
     feed_time = models.TimeField(auto_now=False)
-    feed_date = models.DateField(auto_now_add=True)
     location = models.CharField(max_length=150)
     food_type = models.CharField(max_length=200)
     food_quantity = models.CharField(max_length=250)
