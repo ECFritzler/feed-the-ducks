67cf35109325826f0b9297eb3c232a929dc79ff3
Claire Fritzler <claire@tribot.org>
2021-04-04 17:38:59 -0700

    Remove unnecessary imports

--- frontend/gui/src/components/LogModal.js
+++ frontend/gui/src/components/LogModal.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import TextField from '@material-ui/core/TextField';
 import './LogModal.css'
 
 const LogModal = ({handleClose, show, children}) => {
