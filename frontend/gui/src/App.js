fc98478fc6934796f73b42a3e2c84f8a4341ae4e
Claire Fritzler <claire@tribot.org>
2021-04-05 17:58:31 -0700

    Update app components

--- frontend/gui/src/App.js
+++ frontend/gui/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import CustomLayout from './containers/layout.js'
-import LogList from './containers/LogList.js'
+import CustomLayout from './containers/layout.js';
+import LogList from './containers/LogList.js';
+import About from './components/About.js'
 
 function App() {
   return (
     <div className="App">
         <CustomLayout>
+        <About />
           <LogList/>
         </CustomLayout>
     </div>
