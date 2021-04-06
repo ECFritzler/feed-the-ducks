dbe626b8eaa27661d3524ba98a6bcb5d68b58c40
Claire Fritzler <claire@tribot.org>
2021-04-05 17:56:17 -0700

    Clean up LogModal UI

--- frontend/gui/src/components/LogModal.js
+++ frontend/gui/src/components/LogModal.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './LogModal.css'
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+
 
 const LogModal = ({handleClose, show, children}) => {
   const modalDisplayClass = show ? "modal display-modal" : "modal display-none";
   return(
-    <div className={modalDisplayClass}>
-      <section className="modal-main">
-      <button type="button" onClick={handleClose}>
-        close
-        </button>
-          {children}
-        </section>
-
-    </div>
+      <Box component="div" className={modalDisplayClass}>
+        <Box className="modal-main">
+          <Box display="flex" flexDirection="row-reverse" p={1} m={1}>
+            <Button onClick={handleClose}> x </Button>
+            </Box>
+          <Box p={1} m={1}>
+            {children}
+          </Box>
+        </Box>
+      </Box>
   );
 };
 
